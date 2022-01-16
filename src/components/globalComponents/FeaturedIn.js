0f002f67c64c934a609de8c320ddfba656af812c
FareenaEhtesham <shimmiehtesham229@gmail.com>
2022-01-16 19:55:01 +0500

    Remaining points

--- src/components/globalComponents/FeaturedIn.js
+++ src/components/globalComponents/FeaturedIn.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../../styles/components/globalComponents/featuredIn.scss';
+import { Row, Col } from 'react-bootstrap';
 
 import TwsjLogo from '../../assets/icons/brands-logo/twsj.svg';
 import CnbcLogo from '../../assets/icons/brands-logo/cnbc.svg';
@@ -12,16 +13,40 @@ const FeaturedIn = () => {
   return (
     <div className='featuredIn'>
       <h2 className='container-75 heading-3'>Featured In</h2>
-      <div className='images-wrapper'>
-        <div className='container-75'>
-          <img src={TwsjLogo} alt='TwsjLogo' id='TwsjLogo' />
-          <img src={CnbcLogo} alt='CnbcLogo' id='CnbcLogo' />
-          <img src={TcLogo} alt='TcLogo' id='TcLogo' />
-          <img src={FortuneLogo} alt='FortuneLogo' id='FortuneLogo' />
-          <img src={TnytLogo} alt='TnytLogo' id='TnytLogo' />
-          <img src={VenturebeatLogo} alt='VenturebeatLogo' id='VenturebeatLogo' />
+      <div className='container-75'>
+      <Row className='images-wrapper'>
+          <Col sm={6} md={6} lg={3}>
+            <img src={TwsjLogo} alt='TwsjLogo' id='TwsjLogo' style={{width:"230px"}}/>
+          </Col>
+          <Col sm={6} md={6} lg={3}>
+            <img src={CnbcLogo} alt='CnbcLogo' id='CnbcLogo' />
+          </Col>
+          <Col sm={6} md={6} lg={3}>
+            <img src={TcLogo} alt='TcLogo' id='TcLogo' />
+          </Col>
+          <Col sm={6} md={6} lg={3}>
+            <img src={FortuneLogo} alt='FortuneLogo' id='FortuneLogo' />
+          </Col>
+        </Row>
+        <Row className='images-wrapper'>
+          <Col sm={6} md={6} lg={3}>
+            <img src={TnytLogo} alt='TnytLogo' id='TnytLogo' />
+          </Col>
+          <Col sm={6} md={6} lg={3}>
+            <img src={VenturebeatLogo} alt='VenturebeatLogo' id='VenturebeatLogo' />
+          </Col>
+        </Row>
         </div>
-      </div>
+      {/* <div className='images-wrapper'>
+       <div className='container-75'>
+         <img src={TwsjLogo} alt='TwsjLogo' id='TwsjLogo' />
+         <img src={CnbcLogo} alt='CnbcLogo' id='CnbcLogo' />
+         <img src={TcLogo} alt='TcLogo' id='TcLogo' />
+         <img src={FortuneLogo} alt='FortuneLogo' id='FortuneLogo' />
+         <img src={TnytLogo} alt='TnytLogo' id='TnytLogo' />
+         <img src={VenturebeatLogo} alt='VenturebeatLogo' id='VenturebeatLogo' />
+       </div>
+      </div> */}
     </div>
   );
 };
--- src/components/globalComponents/TrustedBy.js
+++ src/components/globalComponents/TrustedBy.js
@@ -4,18 +4,37 @@ import '../../styles/components/globalComponents/trustedBy.scss';
 import HisenseLogo from '../../assets/icons/trusted/hisense.svg';
 import TclLogo from '../../assets/icons/trusted/tcl.svg';
 import SimplisafeLogo from '../../assets/icons/trusted/simplisafe.svg';
-
+import { Row, Col } from 'react-bootstrap';
 const TrustedBy = () => {
   return (
+    
     <div className='trustedBy'>
       <h2 className='heading-3'>Trusted By</h2>
-      <div className='images-wrapper'>
-        <img src={HisenseLogo} alt='Logo' />
-        <img src={TclLogo} alt='Logo' />
-        <img src={SimplisafeLogo} alt='Logo' />
-        <button>Add any more </button>
-      </div>
+      <Row className='images-wrapper'>
+        <Col sm={6} md={6} lg={3}>
+          <img src={HisenseLogo} alt='Logo' />
+        </Col>
+        <Col sm={6} md={6} lg={3}>
+          <img src={TclLogo} alt='Logo' />
+        </Col>
+        <Col sm={6} md={6} lg={3}>
+          <img src={SimplisafeLogo} alt='Logo' />
+        </Col>
+        <Col sm={6} md={6} lg={3}>
+          <button>Add any more </button>
+        </Col>
+  </Row>
     </div>
+ 
+    // <div className='trustedBy'>
+    //   <h2 className='heading-3'>Trusted By</h2>
+    //   <div className='images-wrapper'>
+    //     <img src={HisenseLogo} alt='Logo' />
+    //     <img src={TclLogo} alt='Logo' />
+    //     <img src={SimplisafeLogo} alt='Logo' />
+    //     <button>Add any more </button>
+    //   </div>
+    // </div>
   );
 };
 
