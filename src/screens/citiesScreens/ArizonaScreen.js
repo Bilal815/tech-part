135f805ecae8f0b962deaa6c5386c6157bf217c7
Bilal Ahmed <62904483+Bilal815@users.noreply.github.com>
2022-03-06 18:37:32 +0500

    Update ArizonaScreen.js

--- src/screens/citiesScreens/ArizonaScreen.js
+++ src/screens/citiesScreens/ArizonaScreen.js
@@ -11,7 +11,7 @@ import CitiesCustomerReviewContainer from '../../components/globalComponents/Cit
 const ArizonaScreen = () => {
   ScrollToTop();
   const breadcrumbList = [
-    { link: '/', label: 'Puls.com', color: '#a1abb0' },
+    { link: '/', label: 'TechNPart.com', color: '#a1abb0' },
     { link: '/cities/az', label: 'Arizona', color: '#017aff' },
   ];
   return (
