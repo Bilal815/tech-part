848fde41a8a78ba0b82b18ca9fbdbc3af4b83f99
Bilal Ahmed <62904483+Bilal815@users.noreply.github.com>
2022-03-06 18:29:03 +0500

    Update MiamiScreen.js

--- src/screens/citiesScreens/MiamiScreen.js
+++ src/screens/citiesScreens/MiamiScreen.js
@@ -13,7 +13,7 @@ import CitiesCustomerReviewContainer from '../../components/globalComponents/Cit
 const MiamiScreen = () => {
   ScrollToTop();
   const breadcrumbList = [
-    { link: '/', label: 'Puls.com', color: '#a1abb0' },
+    { link: '/', label: 'TechNPart.com', color: '#a1abb0' },
     { link: '/cities/fl', label: 'Florida', color: '#a1abb0' },
     { link: '/cities/az/phoenix', label: 'Miami', color: '#017aff' },
   ];
@@ -39,7 +39,7 @@ const MiamiScreen = () => {
       </div>
       <CitiesAboutPulsRepairService city='Miami' />
       <div className='container-70 container-y-50'>
-        <h1 className='heading-1 center my-5'>The Puls Blog</h1>
+        <h1 className='heading-1 center my-5'>The TechNPart Blog</h1>
         <CitiesBlogCardContainer />
       </div>
       <CitiesCustomerReviewContainer city='Miami' />
