5119ed88716d044a1bc2c2b8d599f1026de85bb5
Bilal Ahmed <62904483+Bilal815@users.noreply.github.com>
2022-03-06 18:27:07 +0500

    Update NewyorkScreen.js

--- src/screens/citiesScreens/NewyorkScreen.js
+++ src/screens/citiesScreens/NewyorkScreen.js
@@ -11,7 +11,7 @@ import CitiesCustomerReviewContainer from '../../components/globalComponents/Cit
 const NewyorkScreen = () => {
   ScrollToTop();
   const breadcrumbList = [
-    { link: '/', label: 'Puls.com', color: '#a1abb0' },
+    { link: '/', label: 'TechNPart.com', color: '#a1abb0' },
     { link: '/cities/ny', label: 'New York', color: '#017aff' },
   ];
   return (
