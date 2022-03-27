f4bdd1c6c04907a0066f1ccea75e12458b360f84
FareenaEhtesham <shimmiehtesham229@gmail.com>
2022-03-27 19:58:52 +0500

    Complete QnA integrate with checkout api

--- src/screens/checkout/CheckoutScreen.js
+++ src/screens/checkout/CheckoutScreen.js
@@ -1,7 +1,11 @@
 import React,{useState} from 'react';
 import {Form, Button, Row, Col} from 'react-bootstrap';
 import axios from 'axios';
-import { useDebouncedCallback } from 'use-debounce';
+import AsyncSelect from 'react-select/async';
+// import { useDebouncedCallback } from 'use-debounce';
+import {useHistory} from 'react-router-dom';
+import {toast} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const CheckoutScreen = () => {
 
@@ -12,59 +16,54 @@ const CheckoutScreen = () => {
   const [address, setaddress] = useState("");
   const [arrivalTime, setarrivalTime] = useState("");
   const [arrivalDate, setarrivalDate] = useState("");
-  const [addressSuggestion, setaddressSuggestion] = useState([]);
-  const [showDiv , setshowDiv] = useState(false);
   
-  const fetchAddressSuggestion = useDebouncedCallback( (value) => {
+  let history = useHistory();
+  toast.configure();
 
-    axios({
-      method:"GET",
-      url:`/api/location/${value}`
-    })
-    .then(response => {
-      if(response.data.status === 200){
-        setaddressSuggestion(response.data.response.detail)
-      }
-      else{
-        setaddressSuggestion([])
-      }
-    })   
-    .catch(
-      (error) => {console.log(error);
-        setaddressSuggestion([])
-      }
-      )  
-  },
-  1000
-  )
+  const loadOptions = async(input, callback) => {
 
-  const handleAddressChange = (e) =>{
-    const value = e.target.value;
+    const response = await fetch(`http://localhost:8000/api/location/${input}`);
+    const json = await response.json();
+
+    callback(json.response.detail.map(i => ({
+      label: i.address + " " + i.city + " " + i.country, value: i.address
+    })))
+
+  }
+
+  const handleAddressChange = (value) =>{
     setaddress(value);
-    fetchAddressSuggestion(value)
+    console.log(value)
   }
 
   const onSubmit = (e) =>{
     e.preventDefault();
+    const dataToSend = {
+      first_name: firstName,
+      last_name: lastName,
+      email: email,
+      phone: phone,
+      address: address.value,
+      arrival_time: arrivalTime,
+      arrival_date: arrivalDate,
+    }
+    console.log(dataToSend)
     axios({
       method: "POST",
-      data: {
-        first_name: firstName,
-        last_name: lastName,
-        email: email,
-        phone: phone,
-        address: address,
-        arrival_time: arrivalTime,
-        arrival_date: arrivalDate,
-      },
+      data: dataToSend,
       url: "/api/checkout",
     })
     .then(data => {
       if(data.status === 200){
-      alert(data.data.response.message);
+        toast.success(data.data.response.message);
+        history.push("/");
       }})   
-    .catch(console.log)
+    .catch( err => {
+      toast.error(err.response.data.message);
+      console.log(err);
+    })
   };
+
   return (
       <div style={{'marginTop': '150px'}}>
         <div className='container-65'>
@@ -124,34 +123,17 @@ const CheckoutScreen = () => {
              placeholder="Enter email" />
           </Form.Group>
 
-          <Form.Group className="mb-3" controlId="formBasicAddress">
-            <Form.Label>Address</Form.Label>
-            <Form.Control 
-             type="text" 
-             onChange={handleAddressChange}
-             value={address}
-             placeholder="Enter address" />
-          </Form.Group>
+          <p>Select Address</p>
+          <AsyncSelect 
+            name='address' 
+            value={address}
+            loadOptions={loadOptions}
+            onChange = {handleAddressChange}
+           placeholder="Select Address"
+           defaultOptions={true}
+        />
 
-          {showDiv ?    
-          <div></div>
-          :
-          <Form.Control as="select" 
-          aria-label="Default select example"
-          name="address"
-          onChange={ (e) => {
-            const value = e.target.value;
-            setaddress(value);
-            setshowDiv(true)
-          }}>
-            <option>Select Address</option>
-          {addressSuggestion.map( a => (
-            <option>{a.address} {a.city} {a.country}</option>
-          ))}
-          </Form.Control>}
-          
-          <br/>
-
+        <br/>
           <Form.Group className="mb-3" controlId="formBasicphone">
             <Form.Label>Phone</Form.Label>
             <Form.Control 
